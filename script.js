be5579c35afdc6226d2d69de948af554cff93f2f
Daniel <stuermer.dan137@gmail.com>
2026-07-21 14:46:03 +0200

    included function to highlight specific words in legal notice text

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ const point = document.querySelector('.vector-point');
 btn.addEventListener('click', () => {
     point.classList.toggle('active');
 });
+
+function highlightWords() {
+    const legalNoticeRef = document.getElementById('legalNoticeText');
+    const words = ['Portfolio', 'Developer Akademie GmbH', 'Developer Akademie'];
+
+    let html = legalNoticeRef.innerHTML;
+
+    words.forEach(word => {
+        const regex = new RegExp(word, 'g');
+        html = html.replace(regex, `<span class="highlight">${word}</span>`);
+    });
+
+    legalNoticeRef.innerHTML = html;
+}
+
+highlightWords();
