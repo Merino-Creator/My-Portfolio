a055bcfe4f12c4a6e080783d01423936afbf82d1
Daniel <stuermer.dan137@gmail.com>
2026-07-28 12:16:25 +0200

    linked keyframes css to legal notice file and reworked script to fit for all files // added updated header in legal notice file

--- script.js
+++ script.js
@@ -17,28 +17,30 @@ highlightWords();
 
 let helloBtn = document.getElementById('helloWorldBtn');
 
-helloBtn.onmouseover = () => {
-    helloBtn.textContent = "I´M DANIEL STUERMER";
-    helloBtn.classList.add('expanded');
-    helloBtn.parentElement.classList.add('expanded');
-};
+if (helloBtn) {
+    helloBtn.onmouseover = () => {
+        helloBtn.textContent = "I´M DANIEL STUERMER";
+        helloBtn.classList.add('expanded');
+    };
 
-helloBtn.onmouseout = () => {
-    helloBtn.textContent = "Hello world";
-    helloBtn.classList.remove('expanded');
-    helloBtn.parentElement.classList.remove('expanded');
-};
+    helloBtn.onmouseout = () => {
+        helloBtn.textContent = "Hello world";
+        helloBtn.classList.remove('expanded');
+    };
+}
 
 let joinImgWrapper = document.getElementById('joinImgWrapper');
 let joinImg = document.getElementById('joinImg');
 
-joinImgWrapper.onmouseover = () => {
-    joinImg.style.animationPlayState = 'paused';
-};
+if (joinImgWrapper) {
+    joinImgWrapper.onmouseover = () => {
+        joinImg.style.animationPlayState = 'paused';
+    };
 
-joinImgWrapper.onmouseout = () => {
-    joinImg.style.animationPlayState = 'running';
-};
+    joinImgWrapper.onmouseout = () => {
+        joinImg.style.animationPlayState = 'running';
+    };
+}
 
 let languageBtn = document.getElementById('languageBtn');
 let vectorPoint = document.getElementById('vectorPoint');
@@ -58,27 +60,28 @@ if (languageBtn) {
 }
 
 let sendBtn = document.getElementById('sendBtn');
-let nameInput = document.getElementById('nameInput');
-let emailInput = document.getElementById('emailInput');
-let helpInput = document.getElementById('helpInput');
-let checkbox = document.getElementById('checkbox');
 
-function checkForm() {
-    if (nameInput.value && emailInput.value && helpInput.value && checkbox.checked) {
-        sendBtn.disabled = false;
-    } else {
-        sendBtn.disabled = true;
+if (sendBtn) {
+    let nameInput = document.getElementById('nameInput');
+    let emailInput = document.getElementById('emailInput');
+    let helpInput = document.getElementById('helpInput');
+    let checkbox = document.getElementById('checkbox');
+    let checkmark = document.getElementById('checkmark');
+
+    function checkForm() {
+        if (nameInput.value && emailInput.value && helpInput.value && checkbox.checked) {
+            sendBtn.disabled = false;
+        } else {
+            sendBtn.disabled = true;
+        }
     }
-}
 
-nameInput.oninput = checkForm;
-emailInput.oninput = checkForm;
-helpInput.oninput = checkForm;
-checkbox.onclick = checkForm;
-
-let checkmark = document.getElementById('checkmark');
-
-checkbox.onclick = () => {
-    checkmark.classList.toggle('visible');
-    checkForm();
+    nameInput.oninput = checkForm;
+    emailInput.oninput = checkForm;
+    helpInput.oninput = checkForm;
+    
+    checkbox.onclick = () => {
+        checkmark.classList.toggle('visible');
+        checkForm();
+    };
 }
